ad8b9830f4357b962fc28325630e9b01adf859a4
Ronald Glover <ron881212@gmail.com>
2018-11-12 06:46:47 -0500

    setting up game layout

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,37 @@
 var wins = 0;
 var losses = 0;
+var chances;
 var alreadyGuessed = "";
 
+var animeTitles = [ "one piece", "attack on titan", "my hero academia", "gundam wing", "jojo", "bleach" ]
 
+var newTitle = animeTitles[Math.floor(Math.random() * animeTitles.length)];
 
+var wrongGuess = document.getElementById("alreadyGuessed-text");
+var correctGuess = document.getElementById("correctGuessed-text");
+var totalWins = document.getElementById("wins-text"); 
+var totalLoses = document.getElementById("lost-text");
 
+document.onkeyup = function(event) {
+
+    var wrongGuess = document.getElementById("alreadyGuessed-text");
+    var correctGuess = document.getElementById("correctGuessed-text");
+    var totalWins = document.getElementById("wins-text"); 
+    var totalLoses = document.getElementById("lost-text");
+
+    var userGuess = event.key;
+
+    var newTitle = animeTitles[Math.floor(Math.random() * animeTitles.length)];
+
+    correctGuess.textContent = newTitle;
+    console.log("made it");
+
+    // Write code that will reveal the correct letter or that will place it in the already guess section.
+
+      // Output to the html wins, losses, aready-guessed and chances left
+      userChoiceText.textContent = "Already Guessed: " + userGuess;
+      winsText.textContent = "wins: " + wins;
+      lossesText.textContent = "losses: " + losses;
+    
+
+  };
