81713a95023d381d89228a1c51e0a0e80b1765c8
Ronald Glover <ron881212@gmail.com>
2018-11-12 21:41:27 -0500

    figuring out scope and about to start logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,37 @@
 var wins = 0;
 var losses = 0;
-var chances;
+var chances = 12;
 var alreadyGuessed = "";
 
+
 var animeTitles = [ "one piece", "attack on titan", "my hero academia", "gundam wing", "jojo", "bleach" ]
+var hiddenTitle = [];
 
 var newTitle = animeTitles[Math.floor(Math.random() * animeTitles.length)];
 
-var wrongGuess = document.getElementById("alreadyGuessed-text");
-var correctGuess = document.getElementById("correctGuessed-text");
+
+
+
 var totalWins = document.getElementById("wins-text"); 
 var totalLoses = document.getElementById("lost-text");
+var wrongGuess = document.getElementById("alreadyGuessed-text");
+var correctGuess = document.getElementById("correctGuessed-text");
+var hiddenText = [];
+
+function newGame(){
+    var correctGuess = document.getElementById("correctGuessed-text");
+    var newTitle = animeTitles[Math.floor(Math.random() * animeTitles.length)];
+    correctGuess.textContent = newTitle;
+    var wins = 0;
+    var losses = 0;
+    var chances = 12;
+    var alreadyGuessed = "";
+
+    //All of this is added to see if i can get the show word ppart working
+    for(){
+        
+    }
+};
 
 document.onkeyup = function(event) {
 
@@ -21,17 +42,14 @@ document.onkeyup = function(event) {
 
     var userGuess = event.key;
 
-    var newTitle = animeTitles[Math.floor(Math.random() * animeTitles.length)];
+    if(userGuess === hiddenText){
 
-    correctGuess.textContent = newTitle;
-    console.log("made it");
+    }
 
-    // Write code that will reveal the correct letter or that will place it in the already guess section.
-
-      // Output to the html wins, losses, aready-guessed and chances left
-      userChoiceText.textContent = "Already Guessed: " + userGuess;
-      winsText.textContent = "wins: " + wins;
-      lossesText.textContent = "losses: " + losses;
-    
+    for(let i = 0; i < newTitle.length; i++){
+        // I replaced newTitle[i] with newTitle[_]
+        hiddenTitle.push(newTitle[i]);
+        console.log(hiddenTitle);
+    }
 
   };
