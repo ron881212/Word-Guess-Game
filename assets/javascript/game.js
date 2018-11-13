44303588251b61f8a4c96df84329aa6d716aa770
Ronald Glover <ron881212@gmail.com>
2018-11-13 06:02:27 -0500

    still working out the logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,68 +4,76 @@ var chances = 12;
 var alreadyGuessed = "";
 
 
-var animeTitles = [ "one piece", "attack on titan", "my hero academia", "gundam wing", "jojo", "bleach" ]
+var animeTitles = ["one piece", "attack on titan", "my hero academia", "gundam wing", "jojo", "bleach"]
 
 var newTitle = animeTitles[Math.floor(Math.random() * animeTitles.length)];
 
 
 
 
-var totalWins = document.getElementById("wins-text"); 
+var totalWins = document.getElementById("wins-text");
 var totalLoses = document.getElementById("lost-text");
 var wrongGuess = document.getElementById("alreadyGuessed-text");
 var correctGuess = document.getElementById("correctGuessed-text");
 
-var hiddenText = [];
+// var hiddenText = [];
+var hiddenTitle = [];
+var underLine = [];
 
-function newGame(){
-    var hiddenTitle = [];
-    var underLine = [];
-    
-    var underScore = document.getElementById("underLine-text");
-    var correctGuess = document.getElementById("correctGuessed-text");
-    var newTitle = animeTitles[Math.floor(Math.random() * animeTitles.length)];
+function newGame() {
+    // var hiddenTitle = [];
+    // var underLine = [];
     var chances = 12;
     var alreadyGuessed = "";
 
-    for(let i = 0; i < newTitle.length; i++){
-            // I replaced newTitle[i] with newTitle[_]
-            hiddenTitle.push(newTitle[i]);
-            
-        }
+    var underScore = document.getElementById("underLine-text");
+    var correctGuess = document.getElementById("correctGuessed-text");
+    var newTitle = animeTitles[Math.floor(Math.random() * animeTitles.length)];
 
-    for(let i = 0; i < newTitle.length; i++){
-            // I replaced newTitle[i] with newTitle[_]
-            underLine.push("_");
-        }
-    
-        // underLine.join('');
+
+    for (let i = 0; i < newTitle.length; i++) {
+        hiddenTitle.push(newTitle[i]);
+    }
+
+    for (let i = 0; i < newTitle.length; i++) {
+        underLine.push("_");
+    }
+
 
     correctGuess.textContent = hiddenTitle.join(' ');
     underScore.textContent = underLine.join(' ');
-    console.log(hiddenTitle);
-    console.log(underLine);
 
-    
+    // console.log(hiddenTitle);
+    // console.log(underLine);
+
 };
 
-document.onkeyup = function(event) {
+
+
+document.onkeyup = function (event) {
+    // create a varible to store the amount of items in the hiddinTitle array
+    var test = "a";
 
     var wrongGuess = document.getElementById("alreadyGuessed-text");
     var correctGuess = document.getElementById("correctGuessed-text");
-    var totalWins = document.getElementById("wins-text"); 
+    var totalWins = document.getElementById("wins-text");
     var totalLoses = document.getElementById("lost-text");
-
+    var lives = document.getElementById("lives-text");
     var userGuess = event.key;
 
-    if(userGuess == newTitle) {
+    //create a loop that will unhide keys that are guessed correctly
+
+    //keep working on this logic 
+
+    if ((userGuess === hiddenTitle[0]) || (userGuess === hiddenTitle[1])) {
         console.log("note it");
+
+    } else {
+        chances--;
+        wrongGuess.textContent = "Already Guessed: " += userGuess;
+        lives.textContent = "Guesses you have left: " + chances;
     }
 
-    // for(let i = 0; i < newTitle.length; i++){
-    //     // I replaced newTitle[i] with newTitle[_]
-    //     hiddenTitle.push(newTitle[i]);
-        
-    // }
-    // console.log(hiddenTitle);
-  };
+
+
+};
