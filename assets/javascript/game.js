a05481d222154775e6f337c4bf50059d23773580
Ronald Glover <ron881212@gmail.com>
2018-11-13 04:24:56 -0500

    fixed new game button and added underlines

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,6 @@ var alreadyGuessed = "";
 
 
 var animeTitles = [ "one piece", "attack on titan", "my hero academia", "gundam wing", "jojo", "bleach" ]
-var hiddenTitle = [];
 
 var newTitle = animeTitles[Math.floor(Math.random() * animeTitles.length)];
 
@@ -16,21 +15,38 @@ var totalWins = document.getElementById("wins-text");
 var totalLoses = document.getElementById("lost-text");
 var wrongGuess = document.getElementById("alreadyGuessed-text");
 var correctGuess = document.getElementById("correctGuessed-text");
+
 var hiddenText = [];
 
 function newGame(){
+    var hiddenTitle = [];
+    var underLine = [];
+    
+    var underScore = document.getElementById("underLine-text");
     var correctGuess = document.getElementById("correctGuessed-text");
     var newTitle = animeTitles[Math.floor(Math.random() * animeTitles.length)];
-    correctGuess.textContent = newTitle;
-    var wins = 0;
-    var losses = 0;
     var chances = 12;
     var alreadyGuessed = "";
 
-    //All of this is added to see if i can get the show word ppart working
-    for(){
-        
-    }
+    for(let i = 0; i < newTitle.length; i++){
+            // I replaced newTitle[i] with newTitle[_]
+            hiddenTitle.push(newTitle[i]);
+            
+        }
+
+    for(let i = 0; i < newTitle.length; i++){
+            // I replaced newTitle[i] with newTitle[_]
+            underLine.push("_");
+        }
+    
+        // underLine.join('');
+
+    correctGuess.textContent = hiddenTitle.join(' ');
+    underScore.textContent = underLine.join(' ');
+    console.log(hiddenTitle);
+    console.log(underLine);
+
+    
 };
 
 document.onkeyup = function(event) {
@@ -42,14 +58,14 @@ document.onkeyup = function(event) {
 
     var userGuess = event.key;
 
-    if(userGuess === hiddenText){
-
+    if(userGuess == newTitle) {
+        console.log("note it");
     }
 
-    for(let i = 0; i < newTitle.length; i++){
-        // I replaced newTitle[i] with newTitle[_]
-        hiddenTitle.push(newTitle[i]);
-        console.log(hiddenTitle);
-    }
-
+    // for(let i = 0; i < newTitle.length; i++){
+    //     // I replaced newTitle[i] with newTitle[_]
+    //     hiddenTitle.push(newTitle[i]);
+        
+    // }
+    // console.log(hiddenTitle);
   };
