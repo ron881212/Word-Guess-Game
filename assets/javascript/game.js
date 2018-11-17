a6f5c0a514f7473fb1b960d97586931ba224c467
Ronald Glover <ron881212@gmail.com>
2018-11-17 02:19:50 -0500

    I have to fix repeated incorrect guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,9 +5,10 @@ var alreadyGuessed = "";
 var score = 0;
 var spaces = 0;
 var goldenTicket = 1000;
+var incorrect = [];
 
-
-var animeTitles = ["one-piece", "attack-on-titan", "my-hero-academia", "gundam-wing", "jojo", "bleach"]
+var animeTitles = ["one-piece", "attack-on-titan", "my-hero-academia", "gundam-wing", "cowboy-bebop", "bleach", "yu-yu-hakusho", "astro-boy",
+    "dragonball-z", "hunter-x-hunter", "sword-art-online", "one-punch-man"]
 
 var newTitle = animeTitles[Math.floor(Math.random() * animeTitles.length)];
 
@@ -27,7 +28,8 @@ function newGame() {
 
     hiddenTitle = [];
     underLine = [];
-    
+    incorrect = [];
+
     var totalWins = document.getElementById("wins-text");
     var totalLoses = document.getElementById("lost-text");
     var wrongGuess = document.getElementById("alreadyGuessed-text");
@@ -36,7 +38,7 @@ function newGame() {
 
     var underScore = document.getElementById("underLine-text");
     var correctGuess = document.getElementById("correctGuessed-text");
-    
+
 
     var newTitle = animeTitles[Math.floor(Math.random() * animeTitles.length)];
 
@@ -44,7 +46,7 @@ function newGame() {
     for (let i = 0; i < newTitle.length; i++) {
         hiddenTitle.push(newTitle[i]);
     }
-    
+
     for (let i = 0; i < newTitle.length; i++) {
         if (newTitle[i] === "-") {
             underLine.push("-")
@@ -56,13 +58,13 @@ function newGame() {
     for (let k = 0; k < hiddenTitle.length; k++) {
         if (hiddenTitle[k] === "-") {
             spaces++;
-        } 
+        }
     }
 
     goldenTicket = hiddenTitle.length - spaces;
 
     wrongGuess.textContent = "";
-    
+
     lives.textContent = "Guesses you have left: " + chances;
 
     correctGuess.textContent = hiddenTitle.join(' ');
@@ -74,17 +76,13 @@ function newGame() {
 
 document.onkeyup = function (event) {
 
-    function youWin(){
+    function youWin() {
         newGame();
     }
 
-    function youLoss(){
+    function youLoss() {
         newGame();
     }
-
-    // This variable is the score needed to win
-
-    // var goldenTicket = hiddenTitle.length - spaces;
 
     // Variable I need locally 
 
@@ -97,14 +95,14 @@ document.onkeyup = function (event) {
 
     var input = event.keyCode;
 
-    if(input > 64 && input < 91){
+    if (input > 64 && input < 91) {
         var userGuess = event.key.toLowerCase();
     }
 
     // This loop determines if you guessed correctly
 
     for (let i = 0; i < hiddenTitle.length; i++) {
-        if(userGuess === underLine[i]){
+        if (userGuess === underLine[i]) {
             break;
         }
         if (userGuess === hiddenTitle[i]) {
@@ -112,34 +110,37 @@ document.onkeyup = function (event) {
             console.log("groovy");
             score++;
             underScore.textContent = underLine.join(" ");
-        } 
+        }
     }
 
     // Logic for winning
-    
-        if (score === goldenTicket){
-            wins++;
-            totalWins.textContent = "Wins: " + wins;  
-            setTimeout(youWin, 500); 
-            goldenTicket = 1000;
-        }
-    
+
+    if (score === goldenTicket) {
+        wins++;
+        totalWins.textContent = "Wins: " + wins;
+        setTimeout(youWin, 500);
+        goldenTicket = 1000;
+    }
 
     // Logic for if you guess incorrectly 
 
-        if (hiddenTitle.length > 2 && chances > 0) {
+    for (let j = 13; j > incorrect.length; j--) {
+
+        if (hiddenTitle.length > 2 && userGuess !== incorrect[j]) {
             if ((userGuess !== hiddenTitle[0]) && (userGuess !== hiddenTitle[1]) && (userGuess !== hiddenTitle[2]) && (userGuess !== hiddenTitle[3]) &&
                 (userGuess !== hiddenTitle[4]) && (userGuess !== hiddenTitle[5]) && (userGuess !== hiddenTitle[6]) && (userGuess !== hiddenTitle[7]) &&
                 (userGuess !== hiddenTitle[8]) && (userGuess !== hiddenTitle[9]) && (userGuess !== hiddenTitle[10]) && (userGuess !== hiddenTitle[11]) &&
                 (userGuess !== hiddenTitle[12]) && (userGuess !== hiddenTitle[13]) && (userGuess !== hiddenTitle[14]) && (userGuess !== hiddenTitle[15]) &&
-                (userGuess !== hiddenTitle[16]) && (userGuess !== hiddenTitle[17]) && (userGuess !== hiddenTitle[18]) && (userGuess !== hiddenTitle[19])){
-                
+                (userGuess !== hiddenTitle[16]) && (userGuess !== hiddenTitle[17]) && (userGuess !== hiddenTitle[18]) && (userGuess !== hiddenTitle[19])) {
+
                 console.log("nothing to see here");
                 chances--;
-                wrongGuess.textContent += userGuess;
+                incorrect.push(userGuess);
+                wrongGuess.textContent = incorrect.join("");
                 lives.textContent = "Guesses you have left: " + chances;
             }
         }
+    }
 
     // Logic for losing
 
