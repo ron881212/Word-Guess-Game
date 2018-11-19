ecc446376b8ed25cfb21eed01e5c6136582ea374
Ronald Glover <ron881212@gmail.com>
2018-11-19 15:19:55 -0500

    removing stop function to see if it removes my errors

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -122,33 +122,33 @@ function playHint() {
     }  
 }
 
-function stopHint(){
-    document.body.style.backgroundImage = "url('../assets/images/default-background.jpg')";
-    document.body.style.opacity = "1";
-    var stopTheme1 = document.getElementById("onePiece");
-    stopTheme1.pause();
-    var stopTheme2 = document.getElementById("attack");
-    stopTheme2.pause();
-    var stopTheme3 = document.getElementById("hero");
-    stopTheme3.pause();
-    var stopTheme4 = document.getElementById("gundam");
-    stopTheme4.pause();
-    var stopTheme5 = document.getElementById("cowboy");
-    stopTheme5.pause();
-    var stopTheme6 = document.getElementById("bleach");
-    stopTheme6.pause();
-    var stopTheme7 = document.getElementById("YuYu");
-    stopTheme7.pause();
-    var stopTheme8 = document.getElementById("DBZ");
-    stopTheme8.pause();
-    var stopTheme9 = document.getElementById("hunterx");
-    stopTheme9.pause();
-    var stopTheme10 = document.getElementById("sword");
-    stopTheme10.pause();
-    var stopTheme11 = document.getElementById("punch");
-    stopTheme11.pause();
+// function stopHint(){
+//     document.body.style.backgroundImage = "url('../assets/images/default-background.jpg')";
+//     document.body.style.opacity = "1";
+//     var stopTheme1 = document.getElementById("onePiece");
+//     stopTheme1.pause();
+//     var stopTheme2 = document.getElementById("attack");
+//     stopTheme2.pause();
+//     var stopTheme3 = document.getElementById("hero");
+//     stopTheme3.pause();
+//     var stopTheme4 = document.getElementById("gundam");
+//     stopTheme4.pause();
+//     var stopTheme5 = document.getElementById("cowboy");
+//     stopTheme5.pause();
+//     var stopTheme6 = document.getElementById("bleach");
+//     stopTheme6.pause();
+//     var stopTheme7 = document.getElementById("YuYu");
+//     stopTheme7.pause();
+//     var stopTheme8 = document.getElementById("DBZ");
+//     stopTheme8.pause();
+//     var stopTheme9 = document.getElementById("hunterx");
+//     stopTheme9.pause();
+//     var stopTheme10 = document.getElementById("sword");
+//     stopTheme10.pause();
+//     var stopTheme11 = document.getElementById("punch");
+//     stopTheme11.pause();
    
-}
+// }
 
 // This runs everytime the new game button is clicked
 
